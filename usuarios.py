ab4b0101651e4e3cf96c8eebefc0b5de07dc808b
Jordistt <jordisototripiana@gmail.com>
2024-05-05 12:35:35 +0200

    Branch de Jordi.

--- usuarios.py
+++ usuarios.py
@@ -43,19 +43,17 @@ import csv
 
 
 def registrar_usuario(nombre, contrasena):
-    usuarios = []
     with open('usuarios.csv', mode='a', newline='') as users_file:
         users_writer = csv.writer(users_file, delimiter=';', quotechar='"')
         with open('usuarios.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=';')
-            for row in csv_reader:
-                usuarios.append(row[0])
+            '''for row in csv_reader:
+                usuarios.append(row[0])'''
+            usuarios = [x[0] for x in csv_reader]
         print(f'Los usuarios son: {usuarios}')
-        if nombre not in usuarios:
-            users_writer.writerow((nombre, contrasena))
-            print(f'{nombre},Tu usuario ha sido registrado correctamente')
-        else:
-            print('Ya existe ese usuario, por favor, elige otro nombre.')
+
+        (users_writer.writerow((nombre, contrasena)), print(f'{nombre},Tu usuario ha sido registrado correctamente')) \
+        if nombre not in usuarios else print('Ya existe ese usuario, por favor, elige otro nombre.')
 
 def borrar_usuario(nombre):
     pass
